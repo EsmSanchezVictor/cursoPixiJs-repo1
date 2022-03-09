948ad9c95de226d051caa882652c94c14d1809d9
EsmSanchezVictor <100707976+EsmSanchezVictor@users.noreply.github.com>
2022-03-09 13:24:34 -0300

    movimos la imagen

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,13 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+const clampy: Sprite = Sprite.from("./clampy.png");
 
 clampy.anchor.set(0.5);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+clampy.x = 0
+clampy.y = 0
+/*clampy.x = app.screen.width / 2;
+clampy.y = app.screen.height / 2;*/
 
-app.stage.addChild(clampy);
+app.stage.addChild(clampy); // lo agrega en la pantalla
